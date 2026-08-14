e6c4dc37139a3b0d511d856c3cb09bfc49aa06ec
had3sgames <had3sgames@gmail.com>
2026-08-14 10:01:53 +0000

    components: export shared components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,1 @@
-export { LoginButton } from './Auth/LoginButton'
-export { PortfolioEditor } from './Portfolio/PortfolioEditor'
-export { TemplateList } from './Template/TemplateList'
+export * from './ui'
