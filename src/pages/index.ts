bc2458ab686a12325e2bc14cba6c7b4e0a02e869
had3sgames <had3sgames@gmail.com>
2026-08-14 10:01:53 +0000

    pages: export route pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,4 @@
-export { Home } from './Home'
+export { CreatePortfolioPage } from './CreatePortfolio'
+export { HomePage } from './Home'
 export { PortfolioPage } from './Portfolio'
+export { TemplatesPage } from './Templates'
