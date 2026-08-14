ddfaa5eab4ef57f11263fba5b625a15561d620cd
had3sgames <had3sgames@gmail.com>
2026-08-14 10:01:51 +0000

    ui: export primitives

---
+++ src/components/ui/index.ts
@@ -0,0 +1,8 @@
+export { Badge } from './Badge'
+export { Button } from './Button'
+export { Card } from './Card'
+export { EmptyState } from './EmptyState'
+export { ErrorMessage } from './ErrorMessage'
+export { LoadingState } from './LoadingState'
+export { SectionHeader } from './SectionHeader'
+export { StatCard } from './StatCard'
